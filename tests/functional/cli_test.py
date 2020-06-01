2c2a92fb4592887b1e97b25254c2ab378dc1a8aa
querdos <hamza.essayegh@protonmail.com>
2020-06-01 13:15:23 +0200

    Add mock open

--- tests/functional/cli_test.py
+++ tests/functional/cli_test.py
@@ -3,7 +3,7 @@
 All functional tests related to CLI environment."""
 import pytest
 
-from mock import patch
+from mock import patch, mock_open
 from click.testing import CliRunner
 
 from ssh_metrics import cli
@@ -17,7 +17,8 @@ def version_option_test():
     """Check that the version option return the correct version of the script."""
     runner = CliRunner()
 
-    with runner.isolated_filesystem():
+    m = mock_open()
+    with patch('__main__.open', m):
         result = runner.invoke(cli, ['--version'])
         assert 0 == result.exit_code, result.output
         assert 'X.Y.Z' in result.output
